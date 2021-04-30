83f54afa3cc4f76f47f60dc8f279e5404ae9507a
SorinC6 <kish.sorin@yahoo.com>
2021-04-30 14:23:45 +0300

    added transfer function plus tests

--- contracts/GreatToken.sol
+++ contracts/GreatToken.sol
@@ -10,6 +10,8 @@ contract GreatToken{
 
     mapping(address =>  uint256) public balanceOf;
 
+    event Transfer(address indexed _from, address indexed _to,  uint256 _value);
+
     constructor(uint256 _initalSupply) public {
         balanceOf[msg.sender] = _initalSupply;
         _totalSupply = _initalSupply;
@@ -18,5 +20,18 @@ contract GreatToken{
 
      function totalSupply() public view returns (uint) {
         return _totalSupply;
+    }
+
+    // Transfer
+    function transfer(address _to, uint _value) public returns (bool success){
+    // Exeption if contract doesn't have enough
+    require(balanceOf[msg.sender] >= _value);
+    // transfer of balance
+    balanceOf[msg.sender] -= _value;
+    balanceOf[_to] += _value;
+    // Transfer Event
+    emit Transfer(msg.sender, _to, _value);
+    // return boolean
+    return true;
     }
 }
--- test/GreatToken.js
+++ test/GreatToken.js
@@ -29,4 +29,30 @@ contract("GreatToken" ,(accounts)=> {
             assert.equal(adminBalance.toNumber(),10000000, 'it alocates the inital supply to the admin account')
         })
     })
+
+    it('transfers token ownership', ()=>{
+        return GreatToken.deployed().then((instance)=>{
+            tokenInstance=instance
+            // test require -- sending a larger amount that sender has
+            return tokenInstance.transfer.call(accounts[1], 109999999999);
+        }).then(assert.fail).catch((error)=>{
+            assert(error.message.indexOf('revert') >= 0, 'error message must contain revert')
+            return tokenInstance.transfer.call(accounts[1],25000, {from: accounts[0]})
+        }).then((success)=>{
+            assert.equal(success,true,' it return true for a succesful transfer')
+            return tokenInstance.transfer(accounts[1], 25000, { from: accounts[0] })
+        }).then((receipt)=>{
+            assert.equal(receipt.logs.length,1, 'trigger the event');
+            assert.equal(receipt.logs[0].event,'Transfer', 'first one should be the Transfer event');
+            assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the token are transfered from')
+            assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the token are transfered to')
+            assert.equal(receipt.logs[0].args._value, 25000, 'logs the transfer amount')
+            return tokenInstance.balanceOf(accounts[1]);
+        }).then((balance)=>{
+            assert.equal(balance.toNumber(), 25000, 'adds the amont to receiving account')
+            return tokenInstance.balanceOf(accounts[0])
+        }).then((balance)=>{
+            assert.equal(balance.toNumber(), 9975000 , 'substract the amount from sending account')
+        })
+    })
 })
