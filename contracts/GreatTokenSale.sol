e1a76f5240ef5b1b681cf3afac10572eac202a97
SorinC6 <kish.sorin@yahoo.com>
2021-05-07 14:20:45 +0300

    added buy token function

--- contracts/GreatTokenSale.sol
+++ contracts/GreatTokenSale.sol
@@ -7,10 +7,23 @@ contract GreatTokenSale {
     address admin;
     GreatToken public tokenContract;
     uint256 public tokenPrice;
+    uint256 public tokenSold;
 
     constructor(GreatToken _tokenContract, uint256 _tokenPrice) public {
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
+    }
+
+    // Buy tokens
+    function buyTokens(uint256 _numberOfTokens) public payable {
+        // require that value is equal to token price
+        // require that the contract has enough tokens
+        // require that a transfer in succesful
+
+        // keep track  of numer of token sold
+        tokenSold += _numberOfTokens;
+
+        // Emit Sell Event
     }
 }
--- test/GreatTokenSale.js
+++ test/GreatTokenSale.js
@@ -2,7 +2,9 @@ var GreatTokenSale = artifacts.require("./GreatTokenSale.sol")
 
 contract("GreatTokenSale",(accounts)=>{
     let tokenSaleInstance;
+    let buyer = accounts[1];
     var tokenPrice = 1000000000000000; // wei
+    let numberOfTokens;
 
     it("initializes the contract with the correct values",()=>{
         return GreatTokenSale.deployed().then((instance)=>{
@@ -18,4 +20,17 @@ contract("GreatTokenSale",(accounts)=>{
             assert.equal(price, tokenPrice, 'token price is correct')
         })
     });
+
+    it('facilitates token buying', ()=>{
+        return GreatTokenSale.deployed().then((instance)=>{
+            tokenSaleInstance = instance
+            numberOfTokens = 10;
+            let value = numberOfTokens * tokenPrice
+            return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: value})
+        }).then((receipt)=>{
+            return tokenSaleInstance.tokenSold();
+        }).then((amount)=>{
+            assert.equal(amount.toNumber(), numberOfTokens, ' increments the number of tokens sold')
+        })
+    })
 })
