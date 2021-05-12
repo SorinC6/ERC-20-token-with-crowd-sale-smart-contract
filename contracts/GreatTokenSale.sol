06e06d7f639e23e280fa86646a2ed5fad6211bfa
SorinC6 <kish.sorin@yahoo.com>
2021-05-12 21:16:46 +0300

    added more requires and test for buyTokens functions

--- contracts/GreatTokenSale.sol
+++ contracts/GreatTokenSale.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity >=0.4.20;
 
-import './GreatToken.sol';
+import "./GreatToken.sol";
 
 contract GreatTokenSale {
     address admin;
@@ -9,22 +9,32 @@ contract GreatTokenSale {
     uint256 public tokenPrice;
     uint256 public tokenSold;
 
+    event Sell(address _buyer, uint256 _amount);
+
     constructor(GreatToken _tokenContract, uint256 _tokenPrice) public {
         admin = msg.sender;
         tokenContract = _tokenContract;
         tokenPrice = _tokenPrice;
     }
 
+    // safe math function
+    function multiply(uint256 x, uint256 y) internal pure returns (uint256 z) {
+        require(y == 0 || (z = x * y) / y == x);
+    }
+
     // Buy tokens
     function buyTokens(uint256 _numberOfTokens) public payable {
         // require that value is equal to token price
+        require(msg.value == multiply(_numberOfTokens, tokenPrice));
         // require that the contract has enough tokens
+        require(tokenContract.balanceOf(address(this)) >= _numberOfTokens);
+        
         // require that a transfer in succesful
 
         // keep track  of numer of token sold
         tokenSold += _numberOfTokens;
 
         // Emit Sell Event
-        
+        emit Sell(msg.sender, _numberOfTokens);
     }
 }
--- test/GreatTokenSale.js
+++ test/GreatTokenSale.js
@@ -1,42 +1,59 @@
 var GreatTokenSale = artifacts.require("./GreatTokenSale.sol")
+var GreatToken = artifacts.require("./GreatToken.sol");
 
-contract("GreatTokenSale",(accounts)=>{
+contract("GreatTokenSale", (accounts) => {
     let tokenSaleInstance;
+    let tokenInstance;
+    let admin = accounts[0];
     let buyer = accounts[1];
-    var tokenPrice = 1000000000000000; // wei
+    let tokenPrice = 1000000000000000; // wei
+    let tokensAvailable = 7500000
     let numberOfTokens;
 
-    it("initializes the contract with the correct values",()=>{
-        return GreatTokenSale.deployed().then((instance)=>{
+    it("initializes the contract with the correct values", () => {
+        return GreatTokenSale.deployed().then((instance) => {
             tokenSaleInstance = instance
             return tokenSaleInstance.address
-        }).then((address)=>{
+        }).then((address) => {
             assert.notEqual(address, 0x0, 'has contract address')
             return tokenSaleInstance.tokenContract();
-        }).then((address)=>{
+        }).then((address) => {
             assert.notEqual(address, 0x0, 'has token contract address')
             return tokenSaleInstance.tokenPrice();
-        }).then((price)=>{
+        }).then((price) => {
             assert.equal(price, tokenPrice, 'token price is correct')
         })
     });
 
-    it('facilitates token buying', ()=>{
-        return GreatTokenSale.deployed().then((instance)=>{
-            tokenSaleInstance = instance
+    it('facilitates token buying', () => {
+        return GreatToken.deployed().then(async (instance) => {
+            // Grab token instance first
+            tokenInstance = instance
+            tokenSaleInstance = await GreatTokenSale.deployed();
+            //provide 75% of all tokens to token sale
+            await tokenInstance.transfer(tokenSaleInstance.address, tokensAvailable, { from: admin })
             numberOfTokens = 10;
             let value = numberOfTokens * tokenPrice
-            return tokenSaleInstance.buyTokens(numberOfTokens, {from: buyer, value: value})
-        }).then((receipt)=>{
-            assert.equal(receipt.logs.length,1, 'trigger the event');
-            assert.equal(receipt.logs[0].event,'Transfer', 'first one should be the Transfer event');
-            assert.equal(receipt.logs[0].args._from, accounts[0], 'logs the account the token are transfered from')
-            assert.equal(receipt.logs[0].args._to, accounts[1], 'logs the account the token are transfered to')
-            assert.equal(receipt.logs[0].args._value, 25000, 'logs the transfer amount')
-            
+            return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: value })
+        }).then((receipt) => {
+            assert.equal(receipt.logs.length, 1, 'trigger the event');
+            assert.equal(receipt.logs[0].event, 'Sell', 'should be the "Sell event');
+            assert.equal(receipt.logs[0].args._buyer, buyer, 'logs the account that purchase the token')
+            assert.equal(receipt.logs[0].args._amount, numberOfTokens, 'logs the number of token purchased')
+
             return tokenSaleInstance.tokenSold();
-        }).then((amount)=>{
+        }).then((amount) => {
             assert.equal(amount.toNumber(), numberOfTokens, ' increments the number of tokens sold')
+            return tokenInstance.balanceOf(tokenSaleInstance.address)
+            // try to buy tokens different from ether value
+        }).then((balance) => {
+            assert.equal(balance.toNumber(), tokensAvailable - numberOfTokens, 'check the balance of tokenInstance')
+            return tokenSaleInstance.buyTokens(numberOfTokens, { from: buyer, value: 1 })
+        }).then(assert.fail).catch((error) => {
+            assert(error.message.indexOf('revert') >= 0, 'msg.value must equal number of tokens in wei')
+            return tokenSaleInstance.buyTokens(8000000, { from: buyer, value: numberOfTokens * tokenPrice })
+        }).then(assert.fail).catch((error) => {
+            assert(error.message.indexOf('revert') >= 0, 'cannot purchase more tokens than is available')
         })
     })
 })
